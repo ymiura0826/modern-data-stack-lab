aa67bbc655fbc67b42a89b07aba6c17941b73deb
ymiura0826 <ymiura0826@gmail.com>
2025-07-03 01:51:44 +0900

    dbt_flow reject gcp_creential

--- flows/dbt_flow.py
+++ flows/dbt_flow.py
@@ -1,10 +1,10 @@
 from prefect import flow
 from prefect_dbt.cli.commands import DbtCoreOperation
-from prefect_gcp import GcpCredentials
+#from prefect_gcp import GcpCredentials
 
 @flow(log_prints=True)
 def run_dbt_flow():
-    gcp_credentials = GcpCredentials.load("data-pipeline-sa-key")
+    #gcp_credentials = GcpCredentials.load("data-pipeline-sa-key")
     result = DbtCoreOperation.load("run-dbt-core").run()
     print("dbt run result:", result)
 
