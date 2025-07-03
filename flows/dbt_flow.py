2f344dc51209394b9a5029d2440ba4ebca925a88
ymiura0826 <ymiura0826@gmail.com>
2025-07-03 15:14:00 +0900

    update prefect.yml

--- flows/dbt_flow.py
+++ flows/dbt_flow.py
@@ -1,11 +1,23 @@
-from prefect import flow
-from prefect_dbt.cli.commands import DbtCoreOperation
-#from prefect_gcp import GcpCredentials
+#!/usr/bin/env python3
+# coding: utf-8
 
-@flow(log_prints=True)
+from prefect import flow
+from prefect_gcp.credentials import GcpCredentials
+from prefect_dbt.cli.configs.bigquery import BigQueryTargetConfigs
+from prefect_dbt.cli.credentials import DbtCliProfile
+from prefect_dbt.cli.commands import DbtCoreOperation
+
+@flow(name="run-dbt-on-bigquery", log_prints=True)
 def run_dbt_flow():
-    #gcp_credentials = GcpCredentials.load("data-pipeline-sa-key")
-    result = DbtCoreOperation.load("run-dbt-core").run()
+    gcp_creds: GcpCredentials = GcpCredentials.load("data-pipeline-sa-key")
+    bq_target: BigQueryTargetConfigs = BigQueryTargetConfigs.load("bq-target-configs")
+    dbt_profile: DbtCliProfile = DbtCliProfile.load("dbt-cli-profile")
+    dbt_op: DbtCoreOperation = DbtCoreOperation.load("run-dbt-core")
+    result = dbt_op.run(
+        dbt_cli_profile=dbt_profile,
+        overwrite_profiles=True
+    )
+
     print("dbt run result:", result)
 
 if __name__ == "__main__":
