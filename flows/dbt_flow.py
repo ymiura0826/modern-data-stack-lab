7dc8f27f0d0dc4fbaca91dbf32b4d2d4ac90cd15
ymiura0826 <ymiura0826@gmail.com>
2025-07-03 15:33:29 +0900

    update prefect.yml

--- flows/dbt_flow.py
+++ flows/dbt_flow.py
@@ -2,14 +2,12 @@
 # coding: utf-8
 
 from prefect import flow
-from prefect_gcp.credentials import GcpCredentials
 from prefect_dbt.cli.configs.bigquery import BigQueryTargetConfigs
 from prefect_dbt.cli.credentials import DbtCliProfile
 from prefect_dbt.cli.commands import DbtCoreOperation
 
 @flow(name="run-dbt-on-bigquery", log_prints=True)
 def run_dbt_flow():
-    gcp_creds: GcpCredentials = GcpCredentials.load("data-pipeline-sa-key")
     bq_target: BigQueryTargetConfigs = BigQueryTargetConfigs.load("bq-target-configs")
     dbt_profile: DbtCliProfile = DbtCliProfile.load("dbt-cli-profile")
     dbt_op: DbtCoreOperation = DbtCoreOperation.load("run-dbt-core")
